6394a0cddd69adc9926a91f58b99ffb27c3c6f5e
keldimurodov <keldimurodovsardor@gmail.com>
2024-02-26 02:23:05 +0500

    Added auth.conf and Auth.csv

--- api-gateway/api/handlers/casbin/casbin.go
+++ api-gateway/api/handlers/casbin/casbin.go
@@ -6,6 +6,7 @@ import (
 	"go-user-registr/api-gateway/config"
 	"net/http"
 	"strings"
+	"fmt"
 
 	"github.com/casbin/casbin/v2"
 	"github.com/gin-gonic/gin"
@@ -37,6 +38,41 @@ func (casb *CasbinHandler) CheckCasbinPermission(ca *casbin.Enforcer, conf confi
 				"message": "permission denied",
 			})
 		}
+	}
+
+}
+
+func NewAuthorizer() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		token := ctx.GetHeader("Authorization")
+		if token == "" {
+			return
+		}
+
+		claims, err := tokens.ExtractClaim(token, []byte(config.Load().SignInKey))
+
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"message": "invalid token",
+			})
+			return
+		}
+		sub := claims["role"]
+		obj := ctx.Request.URL.Path
+		etc := ctx.Request.Method
+		fmt.Println(sub, obj, etc)
+		e, _ := casbin.NewEnforcer("auth.conf", "auth.csv")
+		t, _ := e.Enforce(sub, obj, etc)
+
+		fmt.Println(t)
+		if t {
+			ctx.Next()
+			return
+		}
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"message": "net dostupo",
+		})
 	}
 
 }
